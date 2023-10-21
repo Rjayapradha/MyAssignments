5909b434d07e7d6aaa7753a1cb38a2872a3e1810
Kalaiarasi Rajendran <krkalaiarasi@gmail.com>
2023-10-21 11:40:49 +0530

    Add files via upload

---
+++ FindOddNumbers.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		
+		int Maxrange=10;
+		for(int j=1; j<=Maxrange; j++)
+		{
+			if((j%2)==1)
+			{
+			System.out.println(" odd numbers "+ j);
+			}
+		}
+					
+	
+}
+}
